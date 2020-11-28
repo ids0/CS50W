4cf70f4f84796124dc1c901bf112815c99e3d6fc
Ivan <ivandrelichman@gmail.com>
2020-11-28 15:12:53 -0300

    Entry Page completed

--- Project1/wiki/encyclopedia/views.py
+++ Project1/wiki/encyclopedia/views.py
@@ -8,3 +8,29 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request,title):
+
+    # Compara url/entryName con una lista de las entradas en minuscula
+    # if title.lower() in list( map( lambda list_entry: list_entry.lower(), util.list_entries() ) ):
+
+    # Busca la entrada en la lista de entradas, si no encuentra title = ""
+    for correct_title in util.list_entries()+[""]:
+        if correct_title.lower() == title.lower():
+            title = correct_title
+            break
+    # Llego al final de la lista y no encontro
+    if correct_title == "":
+        incorrect_search = title
+        title = correct_title
+    else:
+        incorrect_search = ""
+
+
+
+    print(util.list_entries()+[""])
+    print(f"title is {title}")
+    return render(request, "encyclopedia/entries.html", {
+        "title": title,
+        "incorrect_search": incorrect_search,
+        "entry_content": util.get_entry(title)
+    })
